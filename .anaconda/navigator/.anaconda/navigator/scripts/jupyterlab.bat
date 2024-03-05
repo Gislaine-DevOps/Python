e4c93a2413f9e7fdc59ca0b1f47333906df8fcb4
Gislaine-DevOps <99862018+Gislaine-DevOps@users.noreply.github.com>
2024-03-04 22:19:18 -0600

    Delete .anaconda/navigator directory

--- .anaconda/navigator/.anaconda/navigator/scripts/jupyterlab.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-chcp 1252
-call C:\Users\mgisl\anaconda3\Scripts\activate C:\Users\mgisl\anaconda3
-jupyter lab >C:\Users\mgisl\.anaconda\navigator\.anaconda\navigator\scripts\jupyterlab-out-7.txt 2>C:\Users\mgisl\.anaconda\navigator\.anaconda\navigator\scripts\jupyterlab-err-7.txt
--- .anaconda/navigator/scripts/anaconda-navigator-app.bat
+++
@@ -1,3 +0,0 @@
-chcp 1252
-call C:\Users\mgisl\anaconda3\Scripts\activate C:\Users\mgisl\anaconda3
-anaconda-navigator >C:\Users\mgisl\.anaconda\navigator\scripts\anaconda-navigator-app-out-1.txt 2>C:\Users\mgisl\.anaconda\navigator\scripts\anaconda-navigator-app-err-1.txt
